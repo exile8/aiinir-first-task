3f515f0a6889fba9ab829e727e450ffea8780fb4
Yuliya Pak <pjulie717119@gmail.com>
2023-10-15 00:53:33 +0300

    Implement full functionality with API
    1. Enable making post-request with user_id and query
    2. Implement storing session data in redis db to save memory
    3. Implement peristent storage of recent chat history for every user
    4. Implement moving window of size window_len over chat history of size chat_limit

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from langchain.llms import LlamaCpp
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
-from langchain.memory import ConversationBufferMemory
+from langchain.memory import ConversationBufferMemory, ConversationBufferWindowMemory
 from langchain.document_loaders.directory import DirectoryLoader
 from langchain.document_loaders import (
-    UnstructuredPDFLoader,
+    PyPDFLoader,
     CSVLoader,
 )
-from langchain.text_splitter import CharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import Chroma
 
@@ -15,16 +15,22 @@ from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
 from fastapi import FastAPI
+from langchain.schema import messages_to_dict
+# from langchain.memory.chat_message_histories.in_memory import ChatMessageHistory
+from langchain.memory.chat_message_histories.redis import RedisChatMessageHistory
+from pathlib import Path
+import json
 
 # For development
 callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
 
-pdf_loader = DirectoryLoader('tinkoff-terms', glob='**/*.pdf', loader_cls=UnstructuredPDFLoader)
+# Preparing vectorstore with documents
+pdf_loader = DirectoryLoader('tinkoff-terms', glob='**/*.pdf', loader_cls=PyPDFLoader)
 csv_loader = DirectoryLoader('tinkoff-terms', glob='**/*.csv', loader_cls=CSVLoader)
 
 docs = csv_loader.load() + pdf_loader.load()
 
-text_splitter = CharacterTextSplitter(
+text_splitter = RecursiveCharacterTextSplitter(
     chunk_size=500,
     chunk_overlap=50
 )
@@ -43,13 +49,14 @@ embedding_model = HuggingFaceEmbeddings(
 
 vectorstore = Chroma.from_documents(documents=chunks, embedding=embedding_model)
 
+
 # TODO: Calibrate precision and randomization
 llm = LlamaCpp(
     model_path='llama-2-7b-chat.Q4_K_M.gguf',
     temperature=0.75,
     max_tokens=2000,
     top_p=1,
-    n_ctx=1024,
+    n_ctx=4096,
     callback_manager=callback_manager, 
     verbose=True,
 )
@@ -69,21 +76,44 @@ prompt = PromptTemplate(
     input_variables=['question', 'context', 'chat_history']
 )
 
+chat_limit = 5
+window_len = 3
 
-memory = ConversationBufferMemory(memory_key='chat_history', input_key='question', return_messages=True)
-
-chat = LLMChain(
-    llm=llm,
-    prompt=prompt,
-    memory=memory,
-    verbose=True
-)
+chat_storage = {}
+chat_dir = 'chat-history/'
 
 app = FastAPI()
 
 @app.post('/message')
 def message(user_id : str, message : str):
+    memory = chat_storage.get(
+        user_id,
+        ConversationBufferWindowMemory(
+            memory_key='chat_history',
+            input_key='question',
+            k=window_len,
+            return_messages=True,
+            chat_memory=RedisChatMessageHistory(user_id)
+        )
+    )
+
+    chat = LLMChain(
+        llm=llm,
+        prompt=prompt,
+        memory=memory,
+        verbose=True
+    )
+
     context = vectorstore.max_marginal_relevance_search(message)
     answer = chat.predict(question=message, context=context)
 
-    return {'user_id': user_id, 'answer': answer}
+    if len(memory.chat_memory.messages) > chat_limit * 2:
+        user_file = Path(chat_dir + user_id + '.json')
+        if not user_file.is_file():
+            user_file.touch()
+        with user_file.open('w', encoding='utf-8') as ufp:
+            json.dump(messages_to_dict(memory.chat_memory.messages), ufp, ensure_ascii=False)
+            for i in range(2 * (chat_limit - window_len)):
+                memory.chat_memory.messages.pop(0)
+
+    return {'answer': answer}
